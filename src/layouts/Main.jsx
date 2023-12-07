9d8f9a65a945022ee2bc8975735f66eaa8d390ef
sm_ayman <sultanmdayman@gmail.com>
2023-12-07 23:37:18 +0600

    updated team,project and added rising-stars

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -2,6 +2,7 @@ import Footer from "../pages/Footer/Footer";
 import Home from "../pages/Home/Home";
 import NavBar from "../pages/Navbar/Navbar";
 import Project from "../pages/Project/Project";
+import RisingStars from "../pages/RisingStars/RisingStars";
 import Team from "../pages/Team/Team";
 
 const Main = () => {
@@ -10,7 +11,8 @@ const Main = () => {
       <NavBar></NavBar>
       <Home></Home>
       <Project />
-      <Team/>
+      <Team />
+      <RisingStars/>
       <Footer></Footer>
     </div>
   );
--- src/pages/Team/Team.jsx
+++ src/pages/Team/Team.jsx
@@ -7,7 +7,7 @@ const Team = () => {
   const teamMembers = [
     {
       name: "Md. Hosen Zisad",
-      role: "Software Engineer",
+      role: "Research & Software Engineer",
       image: demo_team,
       social: {
         twitter: "https://twitter.com/johndoe",
@@ -44,7 +44,14 @@ const Team = () => {
   return (
     <section className="bg-gray-100  min-h-screen px-20">
       <div className="container mx-auto">
-        <h2 className="text-4xl font-bold mb-8 ">Our Team</h2>
+        <div className="max-w-3xl mx-auto p-8 text-secondary font-medium text-center">
+          <h2 className="text-4xl font-bold mb-8  text-gray-800">
+            Our Team
+          </h2>
+          <p className="text-gray-600 leading-6">
+            Meet Our Amazing Team Members
+          </p>
+        </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
           {teamMembers.map((member, id) => (
             <TeamMemberCard key={id} member={member} />
